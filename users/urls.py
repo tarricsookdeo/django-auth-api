33e8eab39b7a5c6843c43b8ad1263da596cf124d
Tarric Sookdeo <tarricsookdeo@outlook.com>
2021-09-23 23:35:40 -0400

    Fix URL's to allow for email verification

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,15 @@
+from dj_rest_auth.registration.views import ConfirmEmailView, VerifyEmailView
 from django.urls import include, path
+from django.urls.conf import re_path
 
 urlpatterns = [
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/register/', include('dj_rest_auth.registration.urls'))
+    path('account-confirm-email/<str:key>/', ConfirmEmailView.as_view()),
+    path('', include('dj_rest_auth.urls')),
+    path('register/', include('dj_rest_auth.registration.urls')),
+    path('verify-email/',
+         VerifyEmailView.as_view(), name='rest_verify_email'),
+    path('account-confirm-email/',
+         VerifyEmailView.as_view(), name='account_email_verification_sent'),
+    re_path(r'^account-confirm-email/(?P<key>[-:\w]+)/$',
+            VerifyEmailView.as_view(), name='account_confirm_email'),
 ]
